9cef03ee82b1f9d005370b6144504225ed25baa8
Romain Vanel <romain.vanel@gmail.com>
2014-02-28 16:07:34 +0100

    sortievers

--- extraction_base_demog.sh
+++ extraction_base_demog.sh
@@ -3,6 +3,9 @@ sqlite3 annecy94.bd ".read extract_baptemes.sql"
 sqlite3 annecy94.bd ".read extract_mariages.sql"
 sqlite3 annecy94.bd ".read extract_sepultures.sql"
 
-sed 's/|/;/g' extract_baptemes.txt
-sed 's/|/;/g' extract_mariages.txt
-sed 's/|/;/g' extract_sepultures.txt
+sed 's/|/;/g' extract_baptemes.txt > baptemes.csv
+rm extract_baptemes.txt
+sed 's/|/;/g' extract_mariages.txt > mariages.csv
+rm extract_mariages.txt
+sed 's/|/;/g' extract_sepultures.txt > sepultures.csv
+rm extract_sepultures.txt
